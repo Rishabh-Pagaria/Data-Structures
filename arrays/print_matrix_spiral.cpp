b1472c1d7e47c792255dae92514e69909cf9bb89
Rishabh-Pagaria <rpagaria2000@gmail.com>
2021-06-13 16:28:57 +0530

    good question->take a look at it

---
+++ arrays/print_matrix_spiral.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+// spiral matrix
+// Pro Tip: draw a matrix and then visualize the question and code to make it easy for you
+// complexity : O(m*n)
+int main()
+{
+    int n, m;
+    cin>>n>>m;
+    int a[n][m];
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            cin>>a[i][j];
+        }
+    }
+    int col_start = 0, col_end = m - 1, row_start = 0, row_end = n - 1;
+    while(row_start <= row_end || col_start <= col_end){
+        // for row_start
+        for(int col = col_start; col <= col_end; col++){
+            cout<<a[row_start][col]<<" ";
+        }
+        row_start++;
+        // for col_end
+        for(int row = row_start; row <= row_end; row++){
+            cout<<a[row][col_end]<<" ";
+        }
+        col_end--;
+        // for row_end
+        for(int col = col_end; col >= col_start; col--){
+            cout<<a[row_end][col]<<" ";
+        }
+        row_end--;
+        // for col_start
+        for(int row = col_end; row > col_start; row--){
+            cout<<a[row][col_start]<<" ";
+        }
+        col_start++;
+        
+    }
+    return 0;
+
+}
