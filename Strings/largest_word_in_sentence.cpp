2b3d778d15365a123ed1b45ea9542c3dbc7277d9
Rishabh-Pagaria <rpagaria2000@gmail.com>
2021-06-16 22:41:58 +0530

    easy

---
+++ Strings/largest_word_in_sentence.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    cin.ignore();
+    char arr[n+1];
+    cin.getline(arr,n);
+    cin.ignore();
+
+    int i = 0;
+    int currLen = 0;
+    int maxLen = 0;
+    while(i<=n){
+        if(arr[i] == ' ' || arr[i] == '\0'){
+            if(currLen > maxLen){
+                maxLen = currLen;
+            }
+            currLen = 0;
+        }else{
+            currLen++;
+        }
+        if(arr[i] == '\0'){
+            break;
+        }
+        i++;
+    }
+    cout<<maxLen;
+
+}
