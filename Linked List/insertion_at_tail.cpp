c5bed8df88c6dd560782d7c4af6a084bc30a5ed6
Rishabh-Pagaria <rpagaria2000@gmail.com>
2021-07-01 13:03:14 +0530

    easy (revise this for concept)

---
+++ Linked List/insertion_at_tail.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+// creating the class node 
+class node{
+    public:
+    int data;
+    node* next;
+
+    // constructor
+    node(int val){
+        data = val;
+        next = NULL;
+    }
+};
+
+void insertAtTail(node* &head, int val){
+    node* temp = head;
+    node* n = new node(val);
+    if(head == NULL){
+        head = n;
+        return;
+    }
+    while(temp->next != NULL){
+        temp = temp->next;
+    }
+    temp->next = n;
+
+}
+void displayNode(node* head){
+    while(head!= NULL){
+        cout<<head->data<<"->";
+        head = head->next;
+    }
+    cout<<"NULL";
+}
+int main()
+{
+   
+    node* head = NULL;
+    insertAtTail(head, 1);
+    insertAtTail(head, 3);
+    insertAtTail(head, 4);
+    displayNode(head);
+    return 0;
+} 
